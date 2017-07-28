79b51945d302350e622f601bbfd90189be3c9a5c
FanQinghui <fanqinghui100@126.com>
2017-07-28 16:49:50 +0800

    第三题目录初始化

---
+++ src/main/java/com/fqh/learn/LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,12 @@
+package com.fqh.learn.LongestSubstringWithoutRepeatingCharacters;
+
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        return 0;
+    }
+
+    public static void main(String[] argus) {
+    }
+}
